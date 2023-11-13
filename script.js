b75ae679717ed90c6fba911ce382221576b88843
DELINSHABU <82861020+DELINSHABU@users.noreply.github.com>
2023-11-13 19:49:13 +0530

    React added to the project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Getting body 
+//* Getting body 
 var body = document.querySelector("body")
 
 var color = body.style.backgroundColor ="#FFFFFF"
@@ -7,7 +7,7 @@ var hex =[0,1,2,3,4,5,6,7,8,9,"a","b","c","b","e","f"]
 
 var btn =document.querySelector("button")
 
-// for (var i )
+//* for (var i )
 btn.addEventListener('click',  function(){
     
     let hexColor = "#"
@@ -23,7 +23,27 @@ btn.addEventListener('click',  function(){
 })
 
 
+//* Copy to clipboard 
+document.getElementById("copy").addEventListener('click', () => {
+    let textToCopy = document.getElementById("text-to-copy").innerHTML;
+    
+    // Create a range and select the text
+    var range = document.createRange();
+    range.selectNode(document.getElementById("text-to-copy"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    
+    //* Execute the copy command
+    document.execCommand("copy");
+    
+    //* Deselect the text
+    window.getSelection().removeAllRanges();
 
+    console.log("Text copied: " + textToCopy);
+});
+
+
+//* function for generate color code
 function randomNumber(){
     return Math.floor(Math.random()*hex.length)
 }
